7f6f921b19ea3cd52716380b535b5f900e2495e3
meghan <meghanadhikari4@gmail.com>
2026-03-11 19:39:02 +0545

    prediction

---
+++ prediction.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+data=pd.DataFrame({
+    "subject":[1,2,3,4],
+    "marks":[90,65,78,56]
+})
+X=data[["marks"]]
+Y=data["subject"]
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2)
+model=LinearRegression()
+model.fit(X_train,Y_train)
+prediction=model.predict([[6]])
+print(prediction)
