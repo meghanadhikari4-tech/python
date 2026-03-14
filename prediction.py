1765300541fb6a4759eefe1ad0b2cb755e410b2c
meghan <meghanadhikari4@gmail.com>
2026-03-14 17:54:42 +0545

    Machine learning

--- prediction.py
+++ prediction.py
@@ -4,9 +4,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 data=pd.DataFrame({
     "subject":[1,2,3,4],
-    "marks":[90,65,78,56]
+    "mark":[90,65,78,56]
 })
-X=data[["marks"]]
+X=data[["mark"]]
 Y=data["subject"]
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2)
 model=LinearRegression()
@@ -15,6 +15,6 @@ prediction=model.predict([[6]])
 print(prediction)
 plt.scatter(X,Y,color="blue")
 plt.plot(X,model.predict(X),color="red")
-plt.xlabel("marks")
+plt.xlabel("mark")
 plt.ylabel("subject")
 plt.show()
