9325776563aa5bdc84970913d6ce64bd468dbdb9
meghan <meghanadhikari4@gmail.com>
2026-04-16 23:16:59 +0545

    mean.py

--- mean.py
+++ mean.py
@@ -1,7 +1,7 @@
 import pandas as pd
 clg={
     "Name":['sita','gita','rita','nita'],
-    "Marks":['19','28','39','40']
+    "Marks":['19','29','39','40']
 }
 df=pd.DataFrame(clg)
 df["Marks"]=df['Marks'].astype(int)
