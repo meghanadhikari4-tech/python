f2906c9f71d01e23d40ad27fa2776bc3bc1d41c8
meghan <meghanadhikari4@gmail.com>
2026-06-04 14:55:10 +0545

    time.py

--- time.py
+++ time.py
@@ -5,3 +5,10 @@ start_time=process_time()
 python_list=[i+5 for i in python_list]
 end_time=process_time()
 print(end_time-start_time)
+
+np_array=np.array([i for i in range (1000000)])
+start_time=process_time()
+np_array+=5
+end_time=process_time()
+print(end_time-start_time)
+
